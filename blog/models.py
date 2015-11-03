b40aaeb3dd93cd820dc292327b07ef709f36ae6d
Mithun Radhakrishnan <rkmithuns@gmail.com>
2015-11-03 23:55:14 +0530

    modified templates to display the posts from database

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,42 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+   
+''' how to work on model objects? ''' 
+'''
+
+from django.contrib.auth.models import User
+User.objects.all()
+
+#returns a user model object
+admin=User.objects.get(username='admin')
+
+#select all query; return the django .db.models.query.QuerySet
+Post.objects.all()
+
+#create objects
+Post.objects.create(author=admin,title="my 4th post",text="will this work from CLI",created_date=timezone.now())
+#Filter objects
+Post.objects.filter(author=me)
+#title like %blog%
+Post.objects.filter(title__contains='blog')
+#created_date <= timezone.now()
+Post.objects.filter(created_date__lte=timezone.now())
+
+#get a model object and apply its method
+p=Post.objects.get(title='Python Myenv')
+p.publish()
+Post.objects.filter(published_date__lte=timezone.now())
+#order by
+Post.objects.order_by('created_date')
+# reverse order
+Post.objects.order_by('-created_date')
+
+
+for p in Post.objects.order_by('created_date'): 
+    print "{} {} {}".format(p.author,p.title,p.text)
+
+#chain QuerySets
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+'''
